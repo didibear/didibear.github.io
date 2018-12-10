9da8bf28bb1367d18dbf3dc4860e6e96776be53d
Adrien Turiot <didibear77@gmail.com>
2018-12-10 14:17:58 +0100

    Update brs2018.js

--- SecretSanta/conf/brs2018.js
+++ SecretSanta/conf/brs2018.js
@@ -1,5 +1,5 @@
 var names = ["Camille", "Clemence", "Diane", "Didi", "Dylan", "Gaelle", "Hugo", "Julia", "Kevin", "Louis", "Lucas", "Ludivine", "Manon", "Margot", "Mathieu", "Nikko", "Pierre", "Quentin", "Timothée"];
 var title = "Nowel de la BRS";
-var secret = "carlos";
+var secret = "zizi";
 
 
